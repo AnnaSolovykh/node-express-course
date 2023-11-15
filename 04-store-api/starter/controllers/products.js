09987fdc60961dd739d745cfafb6980a4f733ec7
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-15 16:10:29 +0300

    add select certain field functionality to api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,12 +3,12 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
     const products = await Product
         .find({})
-        .sort('-name');
+        .select('name price');
     res.status(200).json({ products, nbHits: products.length })
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
     
     if (featured) {
@@ -27,6 +27,11 @@ const getAllProducts = async (req, res) => {
         result = result.sort(sortList);
     } else {
         result = result.sort('createdAt');
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
     const products = await result;
     res.status(200).json({ products, nbHits: products.length })
