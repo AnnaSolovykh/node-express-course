2960f3bbb253e5e739b54b435238a1e1870d650b
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-22 16:40:40 +0300

    add jwt token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,18 @@
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
     const { username, password } = req.body;
-    //mongoose validation || JOI || check in the controller
+
     if (!username || !password) {
         throw new CustomAPIError('Please provide email and password', 400);
-    };
-    res.send('Fake Login/register/signup');
+    }
+
+    const id = new Date().getDate();
+    //later in the course id will be provided by db
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'} ); 
+    //payload should be concise, secret shoud be complex
+    res.status(200).json({ msg: 'user created', token })
 };
 
 const dashboard = async (req, res) => {
